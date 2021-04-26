6992d74049d97c2ed00b8ab0436c706662b4dc68
ValsonPereira <valson.pereira@gmail.com>
2021-04-26 19:58:42 -0300

    C5 - Adicao de Conversor de Decimal para Binario

---
+++ src/ConversaoDecimalBinario.java
@@ -0,0 +1,33 @@
+/* Converter um inteiro informado menor que 32 para sua representação em binário */
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ConversaoDecimalBinario {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um numero inteiro ");
+        int valor = leitor.nextInt();
+
+        String binario = "";
+        int resto = 0;
+
+
+        while (valor > 0) {
+            resto = valor % 2;
+            valor = valor / 2;
+
+            binario += resto;
+
+        }
+        String resultado = new StringBuilder(binario).reverse().toString();
+
+        System.out.println(resultado);
+    }
+
+
+}
